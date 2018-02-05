0bc977646048d7ead7b87b2477dab3a776842628
Xiaoyo <grzesiekk15@gmail.com>
2018-02-05 23:58:32 +0100

    test with database

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,18 +1,17 @@
 package bootwildfly;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController
 {
-	@Autowired
-	private HelloWildFlyService service;
+	//	@Autowired
+	//	private HelloWildFlyService service;
 
 	@RequestMapping("hello")
 	public String sayHello()
 	{
-		return service.getHello();
+		return "tt";//service.getHello();
 	}
 }
